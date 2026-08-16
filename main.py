ec62ad3b637bc5b7cf22fc8bac3b6b96cc1e3ea1
OliverG03 <s240371@th-ab.de>
2026-08-16 16:07:00 +0200

    improve listing and choosing pictures

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy
-import pandas
+
 import glob
 
 # Image Files to look up in directory
@@ -7,12 +6,25 @@ DATA_TYPES = ["*.png", "*.jpg", "*.webp", "*.gif"]
 
 # Get Pictures
 pictures = []
-
+choice = None
 for type in DATA_TYPES:
-    pictures += glob.glob(type)
+        pictures += glob.glob(type)
+if not pictures:
+    raise FileNotFoundError("No image files found.\nPlease put the file(s) you want to turn into an ASCII into this folder.")
 
 
 # Present scanned pictures to User 
-print("-------LIST OF ALL PICTURES-------")
+print("-------LIST OF ALL PICTURES-------\n")
 for i, picture in enumerate(pictures, start=1):
     print(f"{i}.    {picture}")
+print("----------------------------------\n")
+print("---------CHOOSE A PICTURE---------\n")
+while not choice:
+    try:
+        choice = int(input("Input the number of the picture you want to turn into an ASCII:\n"))
+        if choice > len(pictures) or choice <= 0:
+            print(f"Choose a number between 1 and {len(pictures)}")
+            choice = None
+    except:
+        raise ValueError("Input a correct value.")
+print(choice)
