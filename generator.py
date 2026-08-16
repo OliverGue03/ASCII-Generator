224afd675676fff0a5a060f62d8871ae1d101bfe
OliverG03 <s240371@th-ab.de>
2026-08-16 18:09:04 +0200

    create generator file and define ascii characters

---
+++ generator.py
@@ -0,0 +1,12 @@
+import PIL.Image
+
+ASCII_CHARACTERS = ["$", "§", "#", "@", "?", "&", "%", "*", ",", ":", ]
+
+def resize_image():
+    pass
+
+def greyscale_image():
+    pass
+
+def generate_ascii(picture):
+    pass
--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 
 import glob
+from generator import generate_ascii
 
 # Image Files to look up in directory
 DATA_TYPES = ["*.png", "*.jpg", "*.webp", "*.gif"]
 
-# Get Pictures
-pictures = []
-choice = None
-for type in DATA_TYPES:
-        pictures += glob.glob(type)
-if not pictures:
-    raise FileNotFoundError("No image files found.\nPlease put the file(s) you want to turn into an ASCII into this folder.")
+def main():
+    # Get Pictures
+    pictures = []
+    choice = None
+
+    for type in DATA_TYPES:
+            pictures += glob.glob(type)
+    if not pictures:
+        raise FileNotFoundError("No image files found.\nPlease put the file(s) you want to turn into an ASCII into this folder.")
 
 
-# Present scanned pictures to User 
-print("-------LIST OF ALL PICTURES-------\n")
-for i, picture in enumerate(pictures, start=1):
-    print(f"{i}.    {picture}")
-    print("0.    EXIT")
-print("----------------------------------\n")
-print("---------CHOOSE A PICTURE---------\n")
-while not choice:
-    try:
-        choice = int(input("Input the number of the picture you want to turn into an ASCII:\n"))
-        if choice == 0:
-             break
-        elif choice > len(pictures) or choice < 0:
-            print(f"Choose a number between 0 and {len(pictures)}")
-            choice = None
-    except:
-        print("Input a correct value.")
-print(choice)
-print("Goodbye!")
+    # Present scanned pictures to User 
+    print("-------LIST OF ALL PICTURES-------\n")
+    for i, picture in enumerate(pictures, start=1):
+        print(f"{i}.    {picture}")
+        print("0.    EXIT")
+    print("----------------------------------\n")
+    print("---------CHOOSE A PICTURE---------\n")
+    while not choice:
+        try:
+            choice = int(input("Input the number of the picture you want to turn into an ASCII:\n"))
+            if choice == 0:
+                print("Goodbye!")
+                break
+            elif choice > len(pictures) or choice < 0:
+                print(f"Choose a number between 0 and {len(pictures)}")
+                choice = None
+        except:
+            print("Input a correct value.")
+
+    # ASCII-fy the image
+    generate_ascii(pictures[choice-1])
+    
+if __name__ == "__main__":
+    main()
